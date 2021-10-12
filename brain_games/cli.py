b96d050fa279502f418474cb9ebed7f010e6eb22
vova <vladimir1012kov@gmail.com>
2021-10-12 21:37:54 +0300

    lint_test changes

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -7,6 +7,3 @@ def welcome_user():
     """test"""
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-
-
-welcome_user()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+
 from brain_games.cli import welcome_user
 
 
 def main():
+    """test"""
     print('Welcome to the Brain Games')
     welcome_user()
 
