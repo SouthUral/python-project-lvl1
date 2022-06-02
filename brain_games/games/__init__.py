16a2ae51c44a888716ee2af236e96cd0a5f566a9
vova <vladimir1012kov@gmail.com>
2022-06-02 21:29:43 +0300

    the data transfer scheme from the module with the game to the engine has been changed

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -1,5 +1,5 @@
 from .even_game import even_game as even # noqa
-from .calculator_game import calculator_game as calc # noqa
+import calculator_game # noqa
 from .gcd_game import gcd_game as gcd # noqa
 from .prime_game import prime_game as prime # noqa
 from .progression_game import progression_game as progression # noqa
--- brain_games/games/calculator_game.py
+++ brain_games/games/calculator_game.py
@@ -2,15 +2,7 @@
 import random
 
 
-def calculator_game():
-    txt_greet = 'What is the result of the expression?'
-    return txt_greet, questions
-
-
-def random_operator():
-    operator_list = ['+', '-', '*']
-    operator = operator_list[random.randint(0, 2)]
-    return operator
+TXT_GREET = 'What is the result of the expression?'
 
 
 def math_result(num_1, num_2, oper):
@@ -22,14 +14,15 @@ def math_result(num_1, num_2, oper):
         return num_1 * num_2
 
 
-def questions():
-    number_1 = random.randint(1, 100)
-    number_2 = random.randint(1, 100)
-    operator = random_operator()
-    if number_1 > number_2:
-        quest_txt = f'{number_1} {operator} {number_2}'
-        try_answer = str(math_result(number_1, number_2, operator))
-    else:
-        quest_txt = f'{number_2} {operator} {number_1}'
-        try_answer = str(math_result(number_2, number_1, operator))
+def give_question():
+    numbers = [random.randint(1, 100), random.randint(1, 100)]
+    numbers.sort(reverse=True)
+    operator = ['+', '-', '*'][random.randint(0, 2)]
+    quest_txt = f'{numbers[0]} {operator} {numbers[1]}'
+    try_answer = str(math_result(*numbers, operator))
     return [quest_txt, try_answer]
+
+
+quest_txt, try_answer = give_question()
+print(quest_txt)
+print(try_answer)
