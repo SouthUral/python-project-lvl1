757dcf6e20c03e2ea34011d4cebb805fa4fc7d26
vova <vladimir1012kov@gmail.com>
2022-06-06 21:57:50 +0300

    in the module progression.py the make_progress_str_try_answer function has been replaced by make_question_line

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,27 +10,25 @@ LOWER_BOUND_LEN = 5
 UPPER_BOUND_LEN = 10
 
 
-def make_progression(init_elem, step, length):
+def make_progression_str(init_elem, step, length):
     progress_list = [init_elem, ]
     for i in range(length - 1):
         prev = progress_list[-1]
         progress_list.append(prev + step)
-    return progress_list
+    return [str(i) for i in progress_list]
 
 
-def make_progress_str_try_answer(item_index, prog_list):
-    progress_list_str = list(map(str, prog_list))
-    try_answer = progress_list_str[item_index]
-    progress_list_str[item_index] = '..'
-    progress_txt = ' '.join(progress_list_str)
-    return progress_txt, try_answer
+def make_question_line(item_index, prog_list):
+    prog_list[item_index] = '..'
+    return ' '.join(prog_list)
 
 
 def get_question_answer():
     diff_const = random.randint(LOWER_BOUND_DIFF, UPPER_BOUND_DIFF)
     init_element = random.randint(LOWER_BOUND_INIT, UPPER_BOUND_INIT)
     len_prog = random.randint(LOWER_BOUND_LEN, UPPER_BOUND_LEN)
-    del_item_index = random.randint(0, len_prog - 1)
-    progress_list = make_progression(init_element, diff_const, len_prog)
-    quest, answer = make_progress_str_try_answer(del_item_index, progress_list)
-    return quest, answer
+    item_index = random.randint(0, len_prog - 1)
+    progress_list = make_progression_str(init_element, diff_const, len_prog)
+    try_answer = progress_list[item_index]
+    question_line = make_question_line(item_index, progress_list)
+    return question_line, try_answer
